66457d86c456407be3c3a46fde59b471fd9a6bd8
dezzydez007 <nkefuan@yahoo.com>
2022-04-05 20:43:03 +0000

    updates

--- Classic Domains/scripts/run.js
+++ Classic Domains/scripts/run.js
@@ -38,11 +38,11 @@ const main = async () => {
   console.log("Contract deployed to:", domainContract.address);
 
   // We're passing in a second variable - value. This is the moneyyyyyyyyyy
-  let txn = await domainContract.register("mortal",  {value: hre.ethers.utils.parseEther('0.1')});
+  let txn = await domainContract.register("njangi",  {value: hre.ethers.utils.parseEther('0.1')});
   await txn.wait();
 
-  const address = await domainContract.getAddress("mortal");
-  console.log("Owner of domain mortal:", address);
+  const address = await domainContract.getAddress("njangi");
+  console.log("Owner of domain njangi:", address);
 
   const balance = await hre.ethers.provider.getBalance(domainContract.address);
   console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
