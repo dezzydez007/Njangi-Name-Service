5a060135298e828d5723f8f4774255eb0c93fd85
dezzydez007 <nkefuan@yahoo.com>
2022-04-06 11:46:16 +0000

    added gitignor and configs in hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,3 +19,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.8.10",
 };
+
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: "0.8.10",
+  networks: {
+    mumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/jLeqZQM_oQ6Lq1yVTgNcAbzX7DHPP7-V",
+      accounts: ["94fbedeb9927f0cad5a4932949ae41697dca9c13b2f341205efc91e64aceb8c0Y"],
+    }
+  }
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,34 @@
+const main = async () => {
+    const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+    const domainContract = await domainContractFactory.deploy("njangi");
+    await domainContract.deployed();
+  
+    console.log("Contract deployed to:", domainContract.address);
+  
+    // CHANGE THIS DOMAIN TO SOMETHING ELSE! I don't want to see OpenSea full of web3verses lol
+    let txn = await domainContract.register("web3verse",  {value: hre.ethers.utils.parseEther('0.1')});
+    await txn.wait();
+    console.log("Minted domain web3verse.njangi");
+  
+    txn = await domainContract.setRecord("web3verse", "Am I a web3verse or a njangi??");
+    await txn.wait();
+    console.log("Set record for web3verse.njangi");
+  
+    const address = await domainContract.getAddress("web3verse");
+    console.log("Owner of domain web3verse:", address);
+  
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+  }
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
